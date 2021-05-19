de78e8677a6f0d38b0062d8b3d765c9fb17dc511
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-05-19 19:50:26 +0530

    [add] common fetch for filters and cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Filters from './components/Filters';
 import Restaurants from './components/Restaurants';
@@ -9,7 +9,7 @@ const Container = styled.div`
   position: relative;
   max-width: 80rem;
   max-height: initial;
-  margin: 0px auto;
+  margin: 0px 10px;
 `
 
 const RestaurantTitle = styled.h1`
@@ -18,12 +18,34 @@ const RestaurantTitle = styled.h1`
 `
 
 function App() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [restaurantData, setResturantData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    async function loadData() {
+      const response = await fetch('https://evening-forest-39803.herokuapp.com/restaurants')
+      const data = await response.json();
+      setResturantData(data);
+      setLoading(false);
+    }
+
+    loadData();
+
+  }, [])
+
+  if (restaurantData.length === 0 || isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <AppProvider>
       <Container>
-        <Filters />
+        <Filters data={restaurantData} />
         <RestaurantTitle>Akola Restaurants</RestaurantTitle>
-        <Restaurants />
+        <Restaurants data={restaurantData} />
       </Container>
     </AppProvider>
   );
