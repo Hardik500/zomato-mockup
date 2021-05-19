36540acc6d0218a2cbf51faf6682125bb512b87c
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-05-19 16:20:18 +0530

    [add] filter helper

---
+++ src/components/Filters/index.js
@@ -0,0 +1,48 @@
+import Filter from "./Filter";
+import { RiArrowUpDownFill } from "react-icons/ri";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: 1.6rem;
+  padding-bottom: 1.6rem;
+  position: sticky;
+  background-color: rgb(255, 255, 255);
+  z-index: 2;
+  transition: top 0.5s ease-in-out 0s;
+`;
+
+export default function Filters() {
+    const filtersAvailable = [
+        {
+            name: "Rating: 4.0+",
+            type: "Button",
+        },
+        {
+            name: "Safe and Hygienic",
+            type: "Button",
+        },
+        {
+            name: "Delivery Time",
+            icon: <RiArrowUpDownFill />,
+            type: "Sort",
+        },
+        {
+            name: "Rating",
+            type: "Range",
+        },
+        {
+            name: "Cost",
+            type: "Range",
+        },
+    ];
+
+    return (
+        <Container>
+            {filtersAvailable.map((properties) => (
+                <Filter key={properties.name} properties={properties} />
+            ))}
+        </Container>
+    );
+}
